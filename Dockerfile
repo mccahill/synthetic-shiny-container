ce3fd4834d1d440efa03e9e2409362caaa2ee07a
Mark McCahill <mark.mccahill@duke.edu>
2016-06-30 16:53:20 -0400

    get Rstudio working well enough to test shiny
    Signed-off-by: Mark McCahill <mark.mccahill@duke.edu>

--- Dockerfile
+++ Dockerfile
@@ -35,7 +35,8 @@ RUN DEBIAN_FRONTEND=noninteractive apt-get install -y \
    python-software-properties \
    wget \
    sudo \
-   libcurl4-openssl-dev 
+   libcurl4-openssl-dev  \
+   libxml2-dev
 
 RUN apt-get update && \
     apt-get upgrade -y
@@ -57,6 +58,98 @@ RUN DEBIAN_FRONTEND=noninteractive wget https://download2.rstudio.org/rstudio-se
 RUN DEBIAN_FRONTEND=noninteractive gdebi -n rstudio-server-0.99.902-amd64.deb
 RUN rm rstudio-server-0.99.902-amd64.deb
 
+
+# R packages we need for devtools - and we need devtools to be able to update the rmarkdown package
+RUN DEBIAN_FRONTEND=noninteractive wget \
+   http://mirrors.nics.utk.edu/cran/src/contrib/rstudioapi_0.6.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/openssl_0.9.4.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/withr_1.0.2.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/brew_1.0-6.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/stringi_1.1.1.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/magrittr_1.5.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/stringr_1.0.0.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/roxygen2_5.0.1.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/rversions_1.0.2.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/git2r_0.15.0.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/devtools_1.12.0.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/R6_2.1.2.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/mime_0.4.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/httr_1.2.0.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/bitops_1.0-6.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/RCurl_1.95-4.8.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/BH_1.60.0-2.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/xml2_1.0.0.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/curl_0.9.7.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/jsonlite_0.9.22.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/downloader_0.4.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/digest_0.6.9.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/memoise_1.0.0.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/Rcpp_0.12.5.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/plyr_1.8.4.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/XML_3.98-1.4.tar.gz \
+   http://mirrors.nics.utk.edu/cran/src/contrib/whisker_0.3-2.tar.gz
+
+RUN DEBIAN_FRONTEND=noninteractive R CMD INSTALL \
+   jsonlite_0.9.22.tar.gz \
+   digest_0.6.9.tar.gz \
+   memoise_1.0.0.tar.gz \
+   whisker_0.3-2.tar.gz \
+   bitops_1.0-6.tar.gz \
+   RCurl_1.95-4.8.tar.gz \
+   Rcpp_0.12.5.tar.gz \
+   plyr_1.8.4.tar.gz \
+   R6_2.1.2.tar.gz \
+   curl_0.9.7.tar.gz \
+   openssl_0.9.4.tar.gz \
+   mime_0.4.tar.gz \
+   httr_1.2.0.tar.gz \
+   rstudioapi_0.6.tar.gz \
+   withr_1.0.2.tar.gz \
+   brew_1.0-6.tar.gz \
+   stringi_1.1.1.tar.gz \
+   magrittr_1.5.tar.gz \
+   stringr_1.0.0.tar.gz \
+   roxygen2_5.0.1.tar.gz \
+   XML_3.98-1.4.tar.gz \
+   BH_1.60.0-2.tar.gz \
+   xml2_1.0.0.tar.gz \
+   rversions_1.0.2.tar.gz \
+   git2r_0.15.0.tar.gz \
+   devtools_1.12.0.tar.gz \
+   downloader_0.4.tar.gz
+
+RUN rm \
+   jsonlite_0.9.22.tar.gz \
+   digest_0.6.9.tar.gz \
+   memoise_1.0.0.tar.gz \
+   whisker_0.3-2.tar.gz \
+   bitops_1.0-6.tar.gz \
+   RCurl_1.95-4.8.tar.gz \
+   Rcpp_0.12.5.tar.gz \
+   plyr_1.8.4.tar.gz \
+   R6_2.1.2.tar.gz \
+   mime_0.4.tar.gz \
+   httr_1.2.0.tar.gz \
+   rstudioapi_0.6.tar.gz \
+   openssl_0.9.4.tar.gz \
+   withr_1.0.2.tar.gz \
+   brew_1.0-6.tar.gz \
+   stringi_1.1.1.tar.gz \
+   magrittr_1.5.tar.gz \
+   stringr_1.0.0.tar.gz \
+   roxygen2_5.0.1.tar.gz \
+   XML_3.98-1.4.tar.gz \
+   BH_1.60.0-2.tar.gz \
+   xml2_1.0.0.tar.gz \
+   curl_0.9.7.tar.gz \
+   rversions_1.0.2.tar.gz \
+   git2r_0.15.0.tar.gz \
+   devtools_1.12.0.tar.gz \
+   downloader_0.4.tar.gz
+
+
+
+
 # Supervisord
 RUN DEBIAN_FRONTEND=noninteractive apt-get install -y supervisor && \
    mkdir -p /var/log/supervisor
@@ -65,9 +158,18 @@ CMD ["/usr/bin/supervisord", "-n"]
 ADD ./conf /additional-configs
 RUN cp /additional-configs/supervisord-shiny.conf /etc/supervisor/conf.d/supervisord-shiny.conf
 
+# Shiny
+RUN R CMD BATCH /additional-configs/install-Shiny.R
 
 # add a non-root user so we can log into R studio as that user; make sure that user is in the group "users"
 RUN adduser --disabled-password --gecos "" --ingroup users guest 
+
+# VerificationMeasures package and dependencies
+RUN DEBIAN_FRONTEND=noninteractive wget http://mirrors.nics.utk.edu/cran/src/contrib/smoothmest_0.1-2.tar.gz
+RUN DEBIAN_FRONTEND=noninteractive R CMD INSTALL smoothmest_0.1-2.tar.gz
+RUN rm smoothmest_0.1-2.tar.gz
+
+RUN cd /additional-configs ; R CMD INSTALL VerificationMeasures_0.1.tar.gz
 
 # add a script that supervisord uses to set the user's password based on an optional
 # environmental variable ($VNCPASS) passed in when the containers is instantiated
---
+++ homedirs/user000/regression-test/server.R
@@ -0,0 +1,239 @@
+library(shiny)
+
+library(VerificationMeasures)
+library("RCurl")
+
+# load original and synthetic data
+setwd("~/")
+#load("cpsOriginal.RData")
+load("/additional-configs/cpsSynthetic.RData")
+
+modelVariables <- names(cpsSynthetic)
+modelOperations = c('+', '-','*', '/')
+modelDependentVariable = "income"
+
+initialUserSpecifiedModel = paste(modelDependentVariable, ' ~ ', 'age', '+', 'I(age^2)', '+', 'educ', '+', 'marital', '+', 'sex')
+initialUserSpecifiedEpsilon = 0.9
+initialUserSpecifiedUnit = 1.0
+initialUserSpecifiedClauses = c('age', '+ I(age^2)', '+ educ', '+ marital', '+ sex')
+
+server <- shinyServer(function(input, output, session) {
+  
+  # track names of elements inserted (so we can remove the newest first)
+  insertedClauseNames <- character(0)
+  # the clause contents
+  #insertedClauses  <- character(0)
+  insertedClauses <<- initialUserSpecifiedClauses
+  
+  userSpecifiedModel = initialUserSpecifiedModel
+  userSpecifiedEpsilon = initialUserSpecifiedEpsilon
+  userSpecifiedUnit = initialUserSpecifiedUnit
+  userSpecifiedDependentVariable = modelDependentVariable
+  
+  output$uiDependentVariable <- renderUI({
+    selectInput("userSpecifiedDependentVariable", "",
+                choices = modelVariables, selected = userSpecifiedDependentVariable,
+                width = "80%" )
+  })
+  
+  output$userSpecifiedDependentVariable <- renderPrint({ 
+    input$userSpecifiedDependentVariable 
+  })
+  
+  observeEvent(input$insertBtn, {
+    oper = isolate(input$userSpecifiedModelOperation)
+    term = isolate(input$userSpecifiedModelTerm)
+    modif = isolate(input$userSpecifiedModelModifier)
+    if( modif == "^2"){
+      term = paste0("I(",term,")",modif)
+    }
+    if( modif == "log"){
+      term = paste0("log(",term,")")
+    }
+    # add the clause to the list of clauses
+    newClause = paste(oper, term)
+    insertedClauses <<- c(insertedClauses, newClause)
+    # send a message to update the model
+    termClauses = paste(insertedClauses, collapse=" ")
+    updateModel = paste(input$userSpecifiedDependentVariable, ' ~ ', termClauses, collapse=" ")
+    aMessage = list(value = updateModel)
+    session$sendInputMessage("userSpecifiedModel", aMessage)
+    
+    btn <- input$insertBtn
+    id <- paste0('rClause', btn)
+    print(btn)
+    insertUI(
+      selector = "#regressionClauses",
+      ui = tags$div(
+        tags$p(newClause),
+        id = id
+      )
+    )
+    insertedClauseNames <<- c(id, insertedClauseNames)
+  })
+  
+  
+  observeEvent(input$removeBtn, {
+    print("removing")
+    print(paste0('#',insertedClauseNames[1]))
+    removeUI(
+      ## pass in appropriate div id
+      selector = paste0('#',insertedClauseNames[1])
+    )
+    insertedClauseNames <<- insertedClauseNames[-1]
+    print("legthn of length(insertedClauses)")
+    print(length(insertedClauses))
+    insertedClauses <<- insertedClauses[-length(insertedClauses)]
+    # send a message to update the model
+    termClauses = paste(insertedClauses, collapse=" ")
+    updateModel = paste(input$userSpecifiedDependentVariable, ' ~ ', termClauses, collapse=" ")
+    aMessage = list(value = updateModel)
+    session$sendInputMessage("userSpecifiedModel", aMessage)
+  })
+  
+  
+  output$uiModelModifier <- renderUI({
+    modifierList = c("   ", "^2", "log")
+    selectInput("userSpecifiedModelModifier", "modifier", 
+                choices = modifierList, selected = "   ", width = "30%" )
+  })
+  
+  output$userSpecifiedModelModifier <- renderPrint({ 
+    input$userSpecifiedModelModifier 
+  })
+  
+  output$uiModelTerm <- renderUI({
+    # remove the dependent variable from the list of possible terms
+    currentList = unlist(lapply(modelVariables, 
+                                function(x) if (x != input$userSpecifiedDependentVariable) { x }))
+    currentList = append('   ', currentList)
+    selectInput("userSpecifiedModelTerm", "term",
+                choices = currentList, selected = "   ", width = "50%" )
+  })
+  
+  output$userSpecifiedModelTerm <- renderPrint({ 
+    input$userSpecifiedModelTerm 
+  })
+  
+  output$uiModelOperation <- renderUI({
+    currentList = append('   ', modelOperations)
+    selectInput("userSpecifiedModelOperation", "operation",
+                choices = currentList, selected = "   ", width = "30%" )
+  })
+  
+  output$userSpecifiedModelOperation <- renderPrint({ 
+    input$userSpecifiedModelOperation 
+  })
+  
+  output$userSpecifiedJobDetail <- renderText({
+    paste("<br>current model:<b>", input$userSpecifiedModel, 
+          "</b><br>epsilon: <b>", input$userSpecifiedEpsilon, 
+          "</b>&nbsp;outcome unit: <b>", input$userSpecifiedUnit, "</b><br><br>")
+  })
+  
+  output$userSpecifiedEpsilon <- renderText({
+    x <- input$userSpecifiedEpsilon
+    print(x)
+  })
+  
+  output$userSpecifiedUnit <- renderText({
+    x <- input$userSpecifiedUnit
+    print(x)
+  })
+  
+  output$userSpecifiedModel <- renderText({
+    x <- input$userSpecifiedModel
+    print(x)
+  })
+  
+  output$usermodelText <- renderText({
+    as.character(input$my_usermodel)
+  })
+  
+  observe({
+    # reset the model whenever the userSpecifiedDependentVariable changes
+    newModel = paste(input$userSpecifiedDependentVariable, ' ~ ')
+    # throw out all the terms
+    insertedClauses <<- character(0) 
+    # Send an update to userSpecifiedModel to reset the value
+    aMessage = list(value = newModel)
+    session$sendInputMessage("userSpecifiedModel", aMessage)
+  })
+  
+  
+  observe({
+    # Run whenever model reset to default button is pressed
+    input$modelReset
+    
+    insertedClauses <<- initialUserSpecifiedClauses
+    # Send an update to userSpecifiedModel to reset the value
+    aMessage = list(value = initialUserSpecifiedModel)
+    session$sendInputMessage("userSpecifiedModel", aMessage)
+  })
+  
+  output$userRemoteResult <- renderText({
+    # add a dependency on the go button
+    input$remoteSubmitButton
+    if (input$remoteSubmitButton == 0)
+      return("")
+    isolate({
+      # remoteResult = getURL("https://streamer.oit.duke.edu/social/messages?access_token=c4450c85bffab33589bbff3c57327325")
+      paste("<br>",  format(Sys.time(), "%a %b %d %X %Y"), 
+            "<br>submitted model:<b>", input$userSpecifiedModel, 
+            "</b><br>epsilon: <b>", input$userSpecifiedEpsilon, 
+            "</b>&nbsp;outcome unit: <b>", input$userSpecifiedUnit, "</b><br>")
+    })
+  })
+  
+  output$userResidualPlot  <- renderPlot({
+    # add a dependency on the go button
+    input$goButton
+    
+    # isolate the dependency on input$userSpecifiedModel
+    theUserModel <- isolate(input$userSpecifiedModel)
+    if (theUserModel!="") { # make sure there is a model
+      # fit the model to the synthetic data
+      reg = lm( theUserModel, data = cpsSynthetic)
+      
+      # Get predictions and residuals for the original data
+      y.hat <- predict(object=reg,newdata = cpsOriginal)
+      residuals <- cpsOriginal$income-y.hat
+      
+      # Creating the output: private residual plot
+      D <- cbind(y.hat, residuals)
+      # epsilon <- 1.0
+      # unit <- 1.0
+      epsilon = isolate(input$userSpecifiedEpsilon)
+      unit = isolate(input$userSpecifiedUnit)
+      nplots = PriRP(D,epsilon,unit)
+      plot(nplots,pch="+",ylab="residuals", xlab="Residuals for original data based on synthetic model")
+    }
+  })
+  
+  
+  output$userSyntheticResidualPlot  <- renderPlot({
+    # add a dependency on the go button
+    input$goButton
+    
+    # isolate the dependency on input$userSpecifiedModel
+    theUserModel <- isolate(input$userSpecifiedModel)
+    if (theUserModel!="") { # make sure there is a model
+      reg = lm( theUserModel, data = cpsSynthetic)
+      
+      # Get predictions and residuals for the synthetic data
+      y.hat <- predict(object=reg,newdata = cpsSynthetic)
+      residuals <- cpsSynthetic$income-y.hat
+      
+      # Creating the output: synthetic data residual plot
+      D <- cbind(y.hat, residuals)
+      
+      # epsilon <- 1.0
+      # unit <- 1.0
+      epsilon = isolate(input$userSpecifiedEpsilon)
+      unit = isolate(input$userSpecifiedUnit)
+      nplots = PriRP(D,epsilon,unit)
+      plot(nplots,pch="*",ylab="residuals", xlab="Residuals for synthetic data based on synthetic model")
+    }
+  })
+  
+})
