a88492e6c3fd37f500dccdc70dc8da85dfe49fa8
Mark McCahill <mark.mccahill@duke.edu>
2016-07-04 16:23:12 -0400

    clean up volume mounts that can be read-only
    Signed-off-by: Mark McCahill <mark.mccahill@duke.edu>

---
+++ shinyapps/ui.R
@@ -0,0 +1,51 @@
+
+library(shiny)
+
+ui <- shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Linear regression model"),
+  # Show a plot of the generated distribution
+  wellPanel( 
+    HTML("<h4>request remote processing</h4>"),
+    htmlOutput("userSpecifiedJobDetail"),
+    actionButton("remoteSubmitButton", "submit for remote processing"),
+    htmlOutput("userRemoteResult")
+  ),
+  wellPanel(
+    
+    #tags$div(id = 'regressionClauses'),
+    fluidRow( column(1, HTML("<h4>design<br>the<br>model</h4>") ), 
+              column(11, 
+                     actionButton("goButton", "plot residuals"),
+                     textInput("userSpecifiedModel", "")
+                     #textOutput("userSpecifiedModel")
+              )
+    ),
+    fluidRow(
+      column(3, wellPanel(HTML("<h5>dependent<br>variable</h5>"), 
+                          uiOutput("uiDependentVariable"),
+                          HTML("<hr>"),
+                          actionButton("modelReset", "Reset to default model"))
+      ),
+      column(2, wellPanel(HTML("<h5>independent<br>variable clauses</h5>"), HTML("<hr>"),
+                          actionButton('insertBtn', 'Add clause'),HTML("<hr>"),
+                          actionButton('removeBtn', 'Remove clause') )
+      ),
+      column(3,  wellPanel(HTML("<h5>define <br> new clause</h5>"), 
+                           uiOutput("uiModelOperation"), 
+                           uiOutput("uiModelTerm"), 
+                           uiOutput("uiModelModifier") )
+      ),  column(4,
+                 wellPanel(
+                   HTML("<hr>"),
+                   sliderInput("userSpecifiedEpsilon", "epsilon", 0.01, 1.0, 0.9, width = '100%' ),
+                   numericInput("userSpecifiedUnit", "outcome unit: ", 1.0, min = 1.0, max = 100.0, width = '30%'))
+      )
+    ),
+    
+    
+    plotOutput("userSyntheticResidualPlot")
+    # plotOutput("userResidualPlot")
+  )
+))
